32e6f5e758caa063ebf01d774cbcae2465317bda
Snehal2608 <shinde1snehal@gmail.com>
2025-04-04 16:02:10 +0530

    Update MenuItem.jsx

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MenuItem = ({ label, onClick }) => {
   return (
     <li
-      className="mt-2 cursor-pointer hover:text-blue-400 py-2 px-3 bg-gray-700 rounded"
+      className="mt-2 cursor-pointer hover:text-blue-400 py-2 px-3 bg-gray-300 rounded"
       onClick={onClick}
     >
       {label}
